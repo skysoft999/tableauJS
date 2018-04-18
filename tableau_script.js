4880c97485a673a45cb38508c056ebdfc08975fd
sanu k yadav <sanu@neunets.com>
2018-04-18 12:04:54 +0530

    Get data from worksheet

---
+++ tableau_script.js
@@ -0,0 +1,36 @@
+ 
+<html>
+<head>
+	<script src="https://public.tableausoftware.com/javascripts/api/tableau-2.min.js " type="text/javascript"></script> 
+</head>
+<body onload=initializeViz();>
+
+<h1>Hello World.</h1>
+<br>
+<br>
+<center><div id='tableauViz'></div></center>
+
+<script>
+var viz, workbook, activeSheet, Worksheet, worksheet;
+function initializeViz(){
+
+var placeholderDiv = document.getElementById("tableauViz");
+
+var url = "https://public.tableau.com/views/Starwars/StarWords";
+var options = {
+   width: "1004",   
+   height: "869",
+   hideTabs: true,
+   hideToolbar: true,
+  onFirstInteractive: function () {
+  workbook = viz.getWorkbook();
+  activeSheet = workbook.getActiveSheet();
+}
+};
+  viz = new tableauSoftware.Viz(placeholderDiv, url, options);
+}
+</script>
+
+
+</body>
+</html>
